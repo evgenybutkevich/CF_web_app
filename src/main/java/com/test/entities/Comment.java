45c4c50838f6ebce162b86505cc5f821d92d04eb
evgenybutkevich <evge_nik@tut.by>
2020-02-16 17:15:29 +0300

    #4_improved_comments

--- src/main/java/com/test/entities/Comment.java
+++ src/main/java/com/test/entities/Comment.java
@@ -1,6 +1,9 @@
 package com.test.entities;
 
 import javax.persistence.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Entity
 @Table(name = "comments")
@@ -10,6 +13,7 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
+    private String date;
     private String text;
 
     @ManyToOne(fetch = FetchType.EAGER)
@@ -20,6 +24,7 @@ public class Comment {
     }
 
     public Comment(String text, User user) {
+        this.date = getStringDate();
         this.text = text;
         this.author = user;
     }
@@ -28,12 +33,28 @@ public class Comment {
         return author.getUsername();
     }
 
+    public String getStringDate() {
+        Date date = new Date();
+        String stringDateFormat = "dd.MM.yyyy HH:mm:ss";
+        DateFormat dateFormat = new SimpleDateFormat(stringDateFormat);
+        String formattedDate = dateFormat.format(date);
+        return formattedDate;
+    }
+
 //--id
     public void setId(Integer id) {
-    this.id = id;
-}
+        this.id = id;
+    }
     public Integer getId() {
         return id;
+    }
+
+//--date
+    public void setDate(String date) {
+        this.date = date;
+    }
+    public String getDate() {
+        return date;
     }
 
 //--text
